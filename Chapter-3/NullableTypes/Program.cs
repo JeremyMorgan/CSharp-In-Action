7f543cc23229fd652b9fd41d88bc093503914c65
Jeremy Morgan <jeremy-morgan@pluralsight.com>
2022-05-14 16:39:23 -0700

    Adding nullable types code

---
+++ Chapter-3/NullableTypes/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// a nullable int
+int? temperature = 77;
+
+
+// if the int has value, do this
+if (temperature.HasValue){
+    Console.WriteLine("It is {0} degrees outside today", temperature.ToString());
+    // otherwise do this
+}else {
+    Console.WriteLine("There is no reading. Perhaps a sensor failed?");
+}
+
+// Regardless of the status of temperature we won't get an exception error. 
